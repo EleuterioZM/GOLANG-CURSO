75fb7c34c35c657b26cc761a3633ff9feb79517e
Eleuterio Zacarias Mabecuane <122021325+EleuterioZM@users.noreply.github.com>
2025-01-24 18:00:07 +0200

    #Feature: Tipos de variaveis go

---
+++ modulo4/tipos.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "fmt"
+
+)
+func main(){
+	//bool
+	//fmt.Printf("Type: %T - value: %v\n", true, true)
+	//string
+	fmt.Printf("Type: %T - value %v\n", "steph", "steph")
+	//int
+	fmt.Printf("Type: %T - value %v\n", 1, 1)
+	//float
+	fmt.Printf("Type: %T - value %v\n", 1.50, 1.50)
+}
